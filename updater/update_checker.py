cc2528af83a16275b03965275bd898f38e98142d
jaewoong <jwsuh25@kis.ac>
2023-08-21 15:54:56 +0900

    Production Redy

--- updater/update_checker.py
+++ updater/update_checker.py
@@ -4,8 +4,10 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 from lastpost import SourceLastID, GhostLastId
+from datetime import datetime as date
 from fetch import FetchUntil
 import schedule
+import time
 
 options = Options()
 options.add_argument("--headless")
@@ -22,39 +24,55 @@ print('Setup Finished')
 
 def sync(ghost, source, name):
     if ghost == source:
-        print(f'There is no update on {name}.')
         return False
-    else:
-        print(f'There is new update on {name} that is not crawled. {ghost} | {source}')
-        print("Start crawling".center(30, '='))
+    print(f'There is new update on {name} that is not crawled. {ghost}/{source}')
+    print("Start crawling".center(30, '='))
 
-        if name == '경복궁':
-            fetch_until.gyeongbokgung(until_id=ghost)
-        if name == '창덕궁':
-            fetch_until.changdeokgung(until_id=ghost)
-        if name == '창경궁':
-            fetch_until.changgyeonggung(until_id=ghost)
-        if name == '덕수궁-공지':
-            fetch_until.deoksugung_notice(until_id=ghost)
-        if name == '덕수궁-채용':
-            fetch_until.deoksugung_career(until_id=ghost)
-        if name == '덕수궁-행사':
-            fetch_until.deoksugung_event(until_id=ghost)
-        if name == '종묘':
-            fetch_until.jongmyo(until_id=ghost)
+    if name == '경복궁':
+        fetch_until.gyeongbokgung(until_id=ghost)
+    if name == '창덕궁':
+        fetch_until.changdeokgung(until_id=ghost)
+    if name == '창경궁':
+        fetch_until.changgyeonggung(until_id=ghost)
+    if name == '덕수궁-공지':
+        fetch_until.deoksugung_notice(until_id=ghost)
+    if name == '덕수궁-채용':
+        fetch_until.deoksugung_career(until_id=ghost)
+    if name == '덕수궁-행사':
+        fetch_until.deoksugung_event(until_id=ghost)
+    if name == '종묘':
+        fetch_until.jongmyo(until_id=ghost)
 
-        print("Crawling finished".center(30, '='))
-        return True
+    print("Crawling finished".center(30, '='))
+    return True
 
 
 def check_update():
-    sync(ghost_last_id.gyeongbokgung(), source_last_id.gyeongbokgung(), '경복궁')
-    sync(ghost_last_id.changdeokgung(), source_last_id.changdeokgung(), '창덕궁')
-    sync(ghost_last_id.changgyeonggung(), source_last_id.changgyeonggung(), '창경궁')
-    sync(ghost_last_id.deoksugung_notice(), source_last_id.deoksugung_notice(), '덕수궁-공지')
-    sync(ghost_last_id.deoksugung_career(), source_last_id.deoksugung_career(), '덕수궁-채용')
-    sync(ghost_last_id.deoksugung_event(), source_last_id.deoksugung_event(), '덕수궁-행사')
-    sync(ghost_last_id.jongmyo(), source_last_id.jongmyo(), '종묘')
+    print(f'[{date.now().strftime("%m/%d %H:%M:%S")}] Checking update')
+    no_update = []
+
+    if not sync(ghost_last_id.gyeongbokgung(), source_last_id.gyeongbokgung(), '경복궁'):
+        no_update.append('경복궁')
+    if not sync(ghost_last_id.changdeokgung(), source_last_id.changdeokgung(), '창덕궁'):
+        no_update.append('창덕궁')
+    if not sync(ghost_last_id.changgyeonggung(), source_last_id.changgyeonggung(), '창경궁'):
+        no_update.append('창경궁')
+    if not sync(ghost_last_id.deoksugung_notice(), source_last_id.deoksugung_notice(), '덕수궁-공지'):
+        no_update.append('덕수궁-공지')
+    if not sync(ghost_last_id.deoksugung_career(), source_last_id.deoksugung_career(), '덕수궁-채용'):
+        no_update.append('덕수궁-채용')
+    if not sync(ghost_last_id.deoksugung_event(), source_last_id.deoksugung_event(), '덕수궁-행사'):
+        no_update.append('덕수궁-행사')
+    if not sync(ghost_last_id.jongmyo(), source_last_id.jongmyo(), '종묘'):
+        no_update.append('종묘')
+
+    if len(no_update) != 0:
+        print(f'No update on: {", ".join(no_update)}')
 
 
-check_update()
+if __name__ == '__main__':
+    check_update()
+    schedule.every(1).minute.do(check_update)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
